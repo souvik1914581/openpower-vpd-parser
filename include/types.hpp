da158da621a5686f84eb2284b9cfb9b92a74e450
Sunny Srivastava <sunnsr25@in.ibm.com>
2023-03-17 12:36:54 -0500

    Common utilty to read/write D-Bus
    The commit provides a common utility method to perform following
    D-Bus operations.
    - Read D-Bus property
    - Write D-Bus property
    - Get object Map for any given object path.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

---
+++ include/types.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <sdbusplus/asio/property.hpp>
+
+namespace vpd
+{
+namespace types
+{
+// This covers mostly all the data type supported over Dbus for a property.
+// clang-format off
+using DbusVariantType = std::variant<
+    std::vector<std::tuple<std::string, std::string, std::string>>,
+    std::vector<std::string>,
+    std::vector<double>,
+    std::string,
+    int64_t,
+    uint64_t,
+    double,
+    int32_t,
+    uint32_t,
+    int16_t,
+    uint16_t,
+    uint8_t,
+    bool,
+    std::vector<uint32_t>,
+    std::vector<uint16_t>,
+    sdbusplus::message::object_path,
+    std::tuple<uint64_t, std::vector<std::tuple<std::string, std::string, double, uint64_t>>>,
+    std::vector<std::tuple<std::string, std::string>>,
+    std::vector<std::tuple<uint32_t, std::vector<uint32_t>>>,
+    std::vector<std::tuple<uint32_t, size_t>>,
+    std::vector<std::tuple<sdbusplus::message::object_path, std::string,
+                           std::string, std::string>>
+ >;
+
+ using MapperGetObject =
+    std::vector<std::pair<std::string, std::vector<std::string>>>;
+
+} // namespace types
+} // namespace vpd
