e6ce9a693be5a3f1c9c91af029520aa1b93852ca
Sunny Srivastava <sunnsr25@in.ibm.com>
2023-04-29 12:38:37 -0500

    Factory pattern to pick correct parser
    The commit interface file to be implemented by all the concrete
    parser classes.
    Also it implements factory pattern to pick appropriate concrete
    class based on the VPD data format.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

---
+++ include/parser_interface.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "types.hpp"
+
+#include <variant>
+
+namespace vpd
+{
+/**
+ * @brief Interface class for parsers.
+ *
+ * Any concrete parser class, implementing parsing logic need to derive from
+ * this interface class and override the parse method declared in this class.
+ */
+class ParserInterface
+{
+  public:
+    /**
+     * @brief Pure virtual function for parsing VPD file.
+     *
+     * The API needs to be overridden by all the classes deriving from parser
+     * inerface class to implement respective VPD parsing logic.
+     *
+     * @return parsed format for VPD data, depending upon the
+     * parsing logic.
+     */
+    virtual std::variant<types::ParsedVPD, types::KeywordVpdMap> parse() = 0;
+
+    /**
+     * @brief Virtual destructor.
+     */
+    virtual ~ParserInterface()
+    {
+    }
+};
+} // namespace vpd
