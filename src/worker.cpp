13440bb32738b75ab05bdfade6a6085c52f1176c
Sunny Srivastava <sunnsr25@in.ibm.com>
2023-07-14 00:54:43 -0500

    Worker class base framework
    The commit implements base framework for worker class.
    The class willexpose API to,
    - select device tree based on the system.
    - process system VPD.
    - process all the FRUs from Inventory json.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

---
+++ src/worker.cpp
@@ -0,0 +1,19 @@
+#include "worker.hpp"
+
+namespace vpd
+{
+void Worker::selectDeviceTreeAndJson()
+{
+    // TODO: Implements logic to select devie tree and JSON.
+}
+
+void Worker::parseAndPublishSystemVPD()
+{
+    // TODO: Implement to parse system VPD.
+}
+
+void Worker::processAllFRU()
+{
+    // TODO: Implement to process all other FRUs.
+}
+} // namespace vpd
