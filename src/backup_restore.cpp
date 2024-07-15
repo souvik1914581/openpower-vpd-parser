2540282cb8c295e0c6032ca98c3b43ba71880e7c
Anupama B R <149049919+branupama@users.noreply.github.com>
2024-07-15 14:58:08 +0530

    Backup and restore IPZ type VPD (#274)
    This commit adds the implementation for backup and restore keyword’s
    for IPZ type VPD.

--- src/backup_restore.cpp
+++ src/backup_restore.cpp
@@ -153,10 +153,191 @@ void BackupAndRestore::backupAndRestoreIpzVpd(types::IPZVpdMap& io_srcVpdMap,
                                               const std::string& i_srcPath,
                                               const std::string& i_dstPath)
 {
-    (void)io_srcVpdMap;
-    (void)io_dstVpdMap;
-    (void)i_srcPath;
-    (void)i_dstPath;
+    if (!m_backupAndRestoreCfgJsonObj["backupMap"].is_array())
+    {
+        logging::logMessage(
+            "Invalid value found for tag backupMap, in backup and restore config JSON.");
+        return;
+    }
+
+    for (const auto& l_aRecordKwInfo :
+         m_backupAndRestoreCfgJsonObj["backupMap"])
+    {
+        const std::string& l_srcRecordName =
+            l_aRecordKwInfo.value("sourceRecord", "");
+        const std::string& l_srcKeywordName =
+            l_aRecordKwInfo.value("sourceKeyword", "");
+        const std::string& l_dstRecordName =
+            l_aRecordKwInfo.value("destinationRecord", "");
+        const std::string& l_dstKeywordName =
+            l_aRecordKwInfo.value("destinationKeyword", "");
+
+        if (l_srcRecordName.empty() || l_dstRecordName.empty() ||
+            l_srcKeywordName.empty() || l_dstKeywordName.empty())
+        {
+            logging::logMessage(
+                "Record or keyword not found in the backup and restore config JSON.");
+            continue;
+        }
+
+        if (!io_srcVpdMap.empty() &&
+            io_srcVpdMap.find(l_srcRecordName) == io_srcVpdMap.end())
+        {
+            logging::logMessage(
+                "Record: " + l_srcRecordName +
+                ", is not found in the source path: " + i_srcPath);
+            continue;
+        }
+
+        if (!io_dstVpdMap.empty() &&
+            io_dstVpdMap.find(l_dstRecordName) == io_dstVpdMap.end())
+        {
+            logging::logMessage(
+                "Record: " + l_dstRecordName +
+                ", is not found in the destination path: " + i_dstPath);
+            continue;
+        }
+
+        types::BinaryVector l_defaultBinaryValue;
+        if (l_aRecordKwInfo.contains("defaultValue") &&
+            l_aRecordKwInfo["defaultValue"].is_array())
+        {
+            l_defaultBinaryValue =
+                l_aRecordKwInfo["defaultValue"].get<types::BinaryVector>();
+        }
+        else
+        {
+            logging::logMessage(
+                "Couldn't read default value for record name: " +
+                l_srcRecordName + ", keyword name: " + l_srcKeywordName +
+                " from backup and restore config JSON file.");
+            continue;
+        }
+
+        bool l_isPelRequired = l_aRecordKwInfo.value("isPelRequired", false);
+
+        types::BinaryVector l_srcBinaryValue;
+        std::string l_srcStrValue;
+        if (!io_srcVpdMap.empty())
+        {
+            genericUtility::getKwVal(io_srcVpdMap.at(l_srcRecordName),
+                                     l_srcKeywordName, l_srcStrValue);
+            l_srcBinaryValue = types::BinaryVector(l_srcStrValue.begin(),
+                                                   l_srcStrValue.end());
+        }
+        else
+        {
+            // Read keyword value from DBus
+            // TODO: Uncomment when readKeyword API implemenation is done,
+            // call the API with required parameters.
+            /* l_srcBinaryValue = utils::readKeyword(i_srcPath,
+            * std::tuple<types::Record, types::Keyword>(l_srcRecordName,
+            l_srcKeywordName)); l_srcStrValue =
+            std::string(l_srcBinaryValue.begin(),
+            l_srcBinaryValue.end());
+            */
+        }
+
+        types::BinaryVector l_dstBinaryValue;
+        std::string l_dstStrValue;
+        if (!io_dstVpdMap.empty())
+        {
+            genericUtility::getKwVal(io_dstVpdMap.at(l_dstRecordName),
+                                     l_dstKeywordName, l_dstStrValue);
+            l_dstBinaryValue = types::BinaryVector(l_dstStrValue.begin(),
+                                                   l_dstStrValue.end());
+        }
+        else
+        {
+            // Read keyword value from DBus
+            // TODO: Uncomment when readKeyword API implemenation is done,
+            // call the API with required parameters.
+            /* l_dstBinaryValue = utils::readKeyword(i_dstPath,
+            * std::tuple<types::Record, types::Keyword>(l_dstRecordName,
+            l_dstKeywordName)); l_dstStrValue =
+            std::string(l_dstBinaryValue.begin(),
+            l_dstBinaryValue.end());
+            */
+        }
+
+        if (l_srcBinaryValue != l_dstBinaryValue)
+        {
+            // ToDo: Handle if there is no valid default value in the backup and
+            // restore config JSON.
+            if (l_dstBinaryValue == l_defaultBinaryValue)
+            {
+                // TODO: Write on hardware,
+                // Write on Dbus,
+                // To keep the data in sync between hardware and parsed map
+                // updating the io_dstVpdMap. This should only be done if write
+                // on hardware returns success. utils::writeKeyword(i_dstPath,
+                // types::IpzData(l_dstRecordName, l_dstKeywordName
+                // l_srcBinaryValue));
+
+                if (!io_dstVpdMap.empty())
+                {
+                    io_dstVpdMap[l_dstRecordName][l_dstKeywordName] =
+                        l_srcStrValue;
+                }
+                continue;
+            }
+
+            if (l_srcBinaryValue == l_defaultBinaryValue)
+            {
+                // TODO: Write on hardware,
+                // Write on Dbus.
+                // utils::writeKeyword(i_srcPath,
+                // types::IpzData(l_srcRecordName, l_srcKeywordName,
+                // l_dstBinaryValue));
+                if (!io_srcVpdMap.empty())
+                {
+                    io_srcVpdMap[l_srcRecordName][l_srcKeywordName] =
+                        l_dstStrValue;
+                }
+            }
+            else
+            {
+                /**
+                 * Update io_srcVpdMap to publish the same data on DBus, which
+                 * is already present on the DBus. Because after calling
+                 * backupAndRestore API the map value will get published to DBus
+                 * in the worker flow.
+                 */
+                if (!io_srcVpdMap.empty() && io_dstVpdMap.empty())
+                {
+                    io_srcVpdMap[l_srcRecordName][l_srcKeywordName] =
+                        l_dstStrValue;
+                }
+
+                // Uncomment when PEL implementation goes in.
+                /*inventory::PelAdditionalData l_additionalData{};
+                std::string l_errorMsg = "Mismatch found between source and
+                destination VPD for record: " + l_srcRecordName + " and
+                keyword: " + l_srcKeywordName;
+                l_additionalData.emplace("DESCRIPTION", l_errorMsg);
+                l_additionalData.emplace("Value read from destination: ",
+                l_dstStrValue); l_additionalData.emplace("Value read from
+                source: ", l_srcStrValue); createPEL(l_additionalData,
+                PelSeverity::WARNING, errIntfForVPDMismatch, nullptr);
+                */
+            }
+        }
+        else if (l_srcBinaryValue == l_defaultBinaryValue &&
+                 l_dstBinaryValue == l_defaultBinaryValue && l_isPelRequired)
+        {
+            std::string l_errorMsg(
+                "Default value found on both source and destination VPD, for record: " +
+                l_srcRecordName + " and keyword: " + l_srcKeywordName);
+
+            // Uncomment when PEL implementation goes in.
+            /*inventory::PelAdditionalData l_additionalData{};
+            l_additionalData.emplace("DESCRIPTION", l_errorMsg);
+            createPEL(l_additionalData,
+            PelSeverity::ERROR, errIntfForVPDDefault, nullptr);
+            */
+            logging::logMessage(l_errorMsg);
+        }
+    }
 }
 
 void BackupAndRestore::setBackupAndRestoreStatus(
