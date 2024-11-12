101f0198d71b24ef66d6305316ab1135e3f99e1b
Anupama B R <anupama.b.r1@ibm.com>
2024-11-12 12:31:43 +0000

    Adding vpd-tool readKeyword stub
    This commit adds,
    1. VpdTool class base infra structure,
    2. readKeyword stub in VpdTool class for reading keyword’s value from
         DBus or from hardware depends on the option selected.
    3. Separate constants.hpp file for vpd-tool.
    4. meson changes to support the above changes.
    
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

---
+++ vpd-tool/include/vpd_tool.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+
+namespace vpd
+{
+/**
+ * @brief Class to support operations on VPD.
+ *
+ * The class provides API's to,
+ * Read keyword value from DBus/hardware.
+ * Update keyword value to DBus/hardware.
+ * Dump DBus object's critical information.
+ * Fix system VPD if any mismatch between DBus and hardware data.
+ * Reset specific system VPD keywords to its default value.
+ * Force VPD collection for hardware.
+ */
+class VpdTool
+{
+  public:
+    /**
+     * @brief Read keyword value.
+     *
+     * API to read VPD keyword's value from the given input path.
+     * If the provided i_onHardware option is true, read keyword's value from
+     * the hardware. Otherwise read keyword's value from DBus.
+     *
+     * Note: In case read keyword's value from DBus, the return value from DBus
+     * call expected as array of bytes.
+     *
+     * @param[in] i_fruPath - DBus object path or EEPROM path.
+     * @param[in] i_recordName - Record name.
+     * @param[in] i_keywordName - Keyword name.
+     * @param[in] i_onHardware - True if i_fruPath is EEPROM path, false
+     * otherwise.
+     * @param[in] i_fileToSave - File path to save keyword's value, if not given
+     * result will redirect to a console.
+     *
+     * @return On success return 0, otherwise return -1.
+     */
+    int readKeyword(const std::string& i_fruPath,
+                    const std::string& i_recordName,
+                    const std::string& i_keywordName, const bool i_onHardware,
+                    const std::string& i_fileToSave = {});
+};
+} // namespace vpd
---
+++ vpd-tool/src/vpd_tool.cpp
@@ -0,0 +1,22 @@
+#include "vpd_tool.hpp"
+
+#include <iostream>
+
+namespace vpd
+{
+int VpdTool::readKeyword(const std::string& i_fruPath,
+                         const std::string& i_recordName,
+                         const std::string& i_keywordName,
+                         const bool i_onHardware,
+                         const std::string& i_fileToSave)
+{
+    // ToDo: Need to add implementation details
+    (void)i_fruPath;
+    (void)i_recordName;
+    (void)i_keywordName;
+    (void)i_onHardware;
+    (void)i_fileToSave;
+
+    return 0;
+}
+} // namespace vpd
