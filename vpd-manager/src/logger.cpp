f2a2d631d9beb0a4538b4b31b9adfc05a7eb5640
Sunny Srivastava <sunnsr25@in.ibm.com>
2024-12-09 22:04:06 -0600

    Shrink details in the log message
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- vpd-manager/src/logger.cpp
+++ vpd-manager/src/logger.cpp
@@ -9,9 +9,12 @@ namespace logging
 void logMessage(std::string_view message, const std::source_location& location)
 {
     std::ostringstream log;
+    log << "FileName: " << location.file_name() << message;
+
+    /* TODO: Check on this later.
     log << "FileName: " << location.file_name() << ","
         << " Line: " << location.line() << ","
-        << " Func: " << location.function_name() << ", " << message;
+        << " Func: " << location.function_name() << ", " << message;*/
 
     std::cout << log.str() << std::endl;
 }
