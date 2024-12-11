0e05c9daf0d10ad87edd79d89d9387fac972982a
Sunny Srivastava <sunnsr25@in.ibm.com>
2024-12-11 00:15:57 -0600

    Optimize logging
    Journal is getting flooded with DBus logs. The change is to remove
    that for the time being.
    Need to revisit.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- vpd-manager/src/logger.cpp
+++ vpd-manager/src/logger.cpp
@@ -9,7 +9,8 @@ namespace logging
 void logMessage(std::string_view message, const std::source_location& location)
 {
     std::ostringstream log;
-    log << "FileName: " << location.file_name() << " " << message;
+    log << "FileName: " << location.file_name() << ","
+        << " Line: " << location.line() << " " << message;
 
     /* TODO: Check on this later.
     log << "FileName: " << location.file_name() << ","
