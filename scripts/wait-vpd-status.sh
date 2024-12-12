547cdf33af7664bac8c8dd68282d00847cdb2744
Anupama B R <anupama.b.r1@ibm.com>
2024-12-12 10:20:56 -0600

    Update sleep in wait-vpd-parser service
    The commit implements wait-vpd-parser service to first waits for 2
    seconds before checking on VPD collection status, it is too early to
    check on the VPD collection status as vpd-manager needs to finish
    collecting all the FRU’s VPD.
    
    Initial delay will avoid early DBus call failure, which is unnecessary.
    
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- scripts/wait-vpd-status.sh
+++ scripts/wait-vpd-status.sh
@@ -3,6 +3,7 @@ retries=100
 echo "Checking every 2s for VPD collection status ...."
 while [ "$retries" -ne 0 ]
 do
+    sleep 2
     output=$(busctl get-property com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager CollectionStatus)
 
     if echo "$output" | grep -q "Completed" ; then
@@ -12,7 +13,6 @@ do
 
     retries="$((retries - 1))"
     echo "Waiting for VPD status update. Retries remaining: $retries"
-    sleep 2
 done
 echo "Exit wait for VPD services to finish with timeout"
 exit 1
