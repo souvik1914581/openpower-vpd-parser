6e60c60abf6ddd5e33c73173e878daaf880e0c7d
Anupama B R <anupama.b.r1@ibm.com>
2025-06-04 04:09:28 -0500

    addoing main script
    Change-Id: Icb0439678d97a586051f75b3ecab330f91f9395c

---
+++ scripts/generate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from generate_valid_eeproms import *
+from generate_corrupted_eeproms import *
+
+VERIFY_FILE_PATH = "/tmp/verify_eeprom.txt"
+
+eepom_info = [("/tmp/valid_eeproms/200-0000/eeprom", "valid_eeprom", "completed", "verify_not_started"),
+              ("/tmp/invalid_eeproms/truncated/eeprom", "invalid_eeprom", "ECC_ERROR", "verify_failed")]
+
+def upadate_verify_info(verify_data):
+    with open(VERIFY_FILE_PATH, "w") as f:
+            json.dump(verify_data, f, indent=4)
+
+def main():
+    fru_data = {}
+
+    #create_sym_link(NEW_SYSTEM_CONFIG_JSON_PATH, SYM_LINK)
+
+    valid_frus_map, valid_verify_data = create_valid_eeproms()
+    invalid_frus_map, invalid_verify_data = createInvalidEeproms()
+
+    fru_data.update(valid_frus_map)
+    fru_data.update(invalid_frus_map)
+
+    add_frus_to_json(fru_data, NEW_SYSTEM_CONFIG_JSON_PATH)
+    upadate_verify_info(valid_verify_data + invalid_frus_map )
+
+if __name__ == "__main__":
+    main()
+
